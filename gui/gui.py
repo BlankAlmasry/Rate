7f74dc937d67227928e3972c7307ca36bc95c499
blankalmasry <blankhussien@gmail.com>
2021-12-12 18:58:47 +0200

    added validation for the ui

--- gui/gui.py
+++ gui/gui.py
@@ -10,27 +10,34 @@ class GUI:
                 'type': 'input',
                 'name': 'file',
                 'message': f'Path to file with matches ({", ".join(supported_readers)}):',
+                'validate': lambda value: value.strip().split('.')[-1] in supported_readers or
+                                          'Please enter a valid file path with a valid extension.'
             },
             {
                 'type': 'input',
                 'name': 'player_a_index',
                 'message': 'First Player Key: (assuming first column is 0, second 1, etc.)',
+                'validate': lambda value: value.strip().isdigit() or 'Please enter a valid number.'
             },
             {
                 'type': 'input',
                 'name': 'player_b_index',
                 'message': 'Second Player Key: (assuming first column is 0, second 1, etc.)',
+                'validate': lambda value: value.strip().isdigit() or 'Please enter a valid number.'
+
             },
             {
                 'type': 'input',
                 'name': 'result_a_index',
                 'message': 'Result for player A: (assuming first column is 0, second 1, etc.)',
+                'validate': lambda value: value.strip().isdigit() or 'Please enter a valid number.'
+
             },
             {
                 'type': 'list',
                 'name': 'algorithm_name',
                 'message': 'Algorithm you want to use to rate the matches:',
-                'choices': supported_algorithms
+                'choices': supported_algorithms,
             },
             {
                 'type': 'list',
@@ -42,16 +49,20 @@ class GUI:
                 'type': 'input',
                 'name': 'result_win',
                 'message': 'Result for a win:(how would a win is typed in the result column)',
+                'validate': lambda value: value.strip() != '' or 'Please enter a valid answer.'
             },
             {
                 'type': 'input',
                 'name': 'result_loss',
                 'message': 'Result for a loss:(how would a loss is typed in the result column)',
+                'validate': lambda value: value.strip() != '' or 'Please enter a valid answer.'
+
             },
             {
                 'type': 'input',
                 'name': 'result_draw',
                 'message': 'Result for a draw:(how would a win is typed in the result column)',
+                'validate': lambda value: value.strip() != '' or 'Please enter a valid answer.'
             },
         ]
         return questions
